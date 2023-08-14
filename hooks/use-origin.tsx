79ef94f44aed161390edd39b50de16b00299c076
Achintha Pallegedara <achinthahirudika@gmail.com>
2023-08-15 00:05:20 +0530

    Add billboards entity

--- hooks/use-origin.tsx
+++ hooks/use-origin.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-export const useOriginal = () => {
+export const useOrigin = () => {
     const[mounted, setMounted] = useState(false);
     const origin = typeof window !== "undefined" && window.location.origin ? window.location.origin :"";
 
